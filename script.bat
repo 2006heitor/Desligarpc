a8e60b02680fa9cd62af35fe1d77b759608d3685
2006heitor <95439880+2006heitor@users.noreply.github.com>
2022-05-05 19:09:52 -0300

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,1 +1,3 @@
 shutdown -t 3600 -s -f -c "Ola Heitor, sou seu assistente virtual do T - Gamer seu Pc sera realizado um shutdown em 7200 Segundos equivalente a 2 Horas se tiver alguma duvida so chamar o !Hr#0681"
+
+shutdown" -a para desligar o shutdown  Comentar para funcionar ctrl + /"
